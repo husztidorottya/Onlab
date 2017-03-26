779af25ec3ddb9e4afe0e61c106b01042d66b233
husztidorottya <dorottya.huszti@chello.hu>
2017-03-26 14:48:00 +0200

    Add files via upload

--- binary_valtoztasd_nyugodtan.py
+++ binary_valtoztasd_nyugodtan.py
@@ -5,54 +5,106 @@ from keras.layers import LSTM
 from keras.callbacks import EarlyStopping
 import argparse
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+"""
+Code words by letters according to one-hot-encoding
+
+Variables:
+   - coded_word: contains a word encoded by one-hot-encoding
+   - letters: dictionary, where key is the letter and value is one-hot-encoded value of the letter
+   - index: value of the letter
+   - Y_set: all labels
+   - x_set: all data set
+   - category: word’s label
+"""
+def code_word(category, data, x_set, Y_set, letters):
+    for line in data:
+            coded_word = []
+            for character in line:
+                index = letters.setdefault(character, len(letters)+1)
+                coded_word.append(index)
+            Y_set.append(category)
+            x_set.append(coded_word)
 
 
+
+"""
+Create padding according to the maximum length in the x_set
+
+Variables:
+   - max_length: maximum length of words in all data set
+"""
+def create_padding(x_set):
+    # find maximum length    
+    max_length = max(x_set, key=len)  
+
+    j = 0
+    
+    # create padding according to max_length
+    for word in x_set:
+        if len(word) != max_length:
+            for i in range(0, len(max_length) - len(word)):
+                x_set[j].append(0)
+        j += 1  
+
+
+
+# Main function
 def main():
     letters = dict()
     x_train = [] 
+    x_set = []
+    Y_set = []
+    x_test = []
     Y_train = []
+    Y_test = []
 
+    # read command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('category_one_inputname')
     parser.add_argument('category_two_inputname')
     args = parser.parse_args()
 
+    # read words from one file
     with open(args.category_one_inputname) as file:
-        for line in file:
-            coded_word = []
-            for character in line:
-                index = letters.setdefault(character,len(letters)+1)
-                coded_word.append(index)
-            Y_train.append(0)
-            x_train.append(coded_word)
+        data = file.read().split('\n')
+   
+    # encoding words and append to dataset and labels
+    code_word(0, data, x_set, Y_set, letters)
 
-    #leghosszabb szo megtalalasa    
-    max_length = max(x_train,key=len)
+    # read words from the other file
+    with open(args.category_two_inputname) as file:
+        data2 = file.read().split('\n')
+   
+    # encoding words and append to dataset and labels
+    code_word(1, data2, x_set, Y_set, letters)
 
-    j = 0
-    
-    #referenciahossznak megfelelo padding kepzese
-    for word in x_train:
-        if len(word)!=max_length:
-            for i in range(0,len(max_length)-len(word)):
-                x_train[j].append(0)
-        j += 1        
-    
+    create_padding(x_set)
+
+    # split randomly x_set to train and test dataset (90% train + 10% test)
+    x_train, x_test, Y_train, Y_test = train_test_split(x_set, Y_set, test_size = 0.1)
+
     X_train = np.array(x_train)
-
-    #max ennyifele erteket vehet fel
+    X_test = np.array(x_test)
+    
+    # max number of values
     number = len(letters)+1
 
+    # defining model
     model = Sequential()
-    model.add(Embedding(number, 10))
-    model.add(LSTM(output_dim = 10, activation = 'sigmoid', inner_activation = 'hard_sigmoid'))
+    model.add(Embedding(number, 100))
+    model.add(LSTM(output_dim = 100, activation = 'sigmoid', inner_activation = 'hard_sigmoid'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
 
+    # train and test, using earlyStopping to stop after 1 rounds if not improving
     model.compile(loss = 'binary_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
-    earlyStopping = EarlyStopping(monitor='val_loss',patience=0,verbose=0,mode='auto')
-    model.fit(X_train, Y_train, batch_size = 16, nb_epoch = 10, callbacks=[earlyStopping],validation_split=0.1)
+    earlyStopping = EarlyStopping(monitor = 'val_loss', patience = 1, verbose = 0, mode = 'auto')
+    model.fit(X_train, Y_train, batch_size = 16, nb_epoch = 10, callbacks = [earlyStopping], validation_split = 0.1)
+    score = model.evaluate(X_test, Y_test, batch_size = 16)
+    print(score)
 
 if __name__ == '__main__':
     main()
